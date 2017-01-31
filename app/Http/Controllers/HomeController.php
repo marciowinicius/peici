43d2c3658e8fcae5c8c6a2252f5386913d53d70d
marcio.winicius <marciowinicius.mw@gmail.com>
2017-01-31 12:57:34 -0200

    Adicao de algumas views.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tipoUsuario = Auth::user()->tipo;
+        if ($tipoUsuario = 'Aluno'){
+            return view('aluno/home');
+        } elseif ($tipoUsuario = 'Professor'){
+            return view('professor/home');
+        } elseif ($tipoUsuario = 'Empresa') {
+            return view('empresa/home');
+        } elseif ($tipoUsuario = 'Administracao') {
+            return view('administracao/home');
+        } else {
+            return redirect('/');
+        }
     }
 }
